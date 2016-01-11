fc75e8e5fe3a9aa92ca2ba57192d1bfe8c10931d
Ander Ipartek <auraga@ipartek.com>
2016-01-11 17:42:55 +0100

    4.4. Añadir una ubicación para los mensajes

---
+++ src/test/java/com/companyname/springapp/web/InventoryControllerTests.java
@@ -0,0 +1,29 @@
+package com.companyname.springapp.web;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Map;
+
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.companyname.springapp.service.SimpleProductManager;
+
+public class InventoryControllerTests {
+
+	@Test
+	public void test() throws Exception {
+		InventoryController controller = new InventoryController();
+		controller.setProductManager(new SimpleProductManager());
+
+		ModelAndView modelAndView = controller.handleRequest(null, null);
+		assertEquals("hello", modelAndView.getViewName());
+		assertNotNull(modelAndView.getModel());
+		@SuppressWarnings("unchecked")
+		Map<String, Object> modelMap = (Map<String, Object>) modelAndView.getModel().get("model");
+        String nowValue = (String) modelMap.get("now");
+        assertNotNull(nowValue);
+	}
+
+}
